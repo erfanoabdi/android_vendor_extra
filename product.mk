ad07a0f6085d678692c9e60d1ae9966dd5ba1c4b
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-04-27 23:16:01 -0500

    extra: disable dex preopt
    Change-Id: I00cc2787c94058d6a69d2eab77deb330e620fb9e

--- product.mk
+++ product.mk
@@ -4,8 +4,4 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 PRODUCT_PACKAGES += \
     Snap
 
-# Dex Preopt
-WITH_DEXPREOPT := true
-DONT_DEXPREOPT_PREBUILTS := true
-
 $(call prepend-product-if-exists, vendor/invisiblek/product.mk)
