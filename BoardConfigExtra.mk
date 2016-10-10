701d27f5782d18ba196cd93c364fbaa77eb1b384
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-10-10 10:54:17 -0500

    extra: build with sdclang
    Change-Id: I6908783dfa984fc2428e5820b56cef8b69a9d98c

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,1 +1,7 @@
 -include vendor/invisiblek/BoardConfigExtra.mk
+
+ifneq ($(HOST_OS),darwin)
+SDCLANG := true
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
+SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+endif
