d3404daed0979ec1ce48bf62bcc13c8a2a35b349
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-08 15:13:50 -0600

    Revert "extra: build with sdclang"
    This reverts commit 701d27f5782d18ba196cd93c364fbaa77eb1b384.

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,7 +1,1 @@
 -include vendor/invisiblek/BoardConfigExtra.mk
-
-ifneq ($(HOST_OS),darwin)
-SDCLANG := true
-SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
-SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
-endif
