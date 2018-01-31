d112408fd0fdc3549a7a7027eac8fab980059e15
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-30 22:33:33 -0600

    extra: add a service that sets a device's ttl based on a persist prop

--- product.mk
+++ product.mk
@@ -5,6 +5,9 @@ $(call prepend-product-if-exists, vendor/invisiblek/product.mk)
 
 TARGET_EXTRA_KERNEL_MODULES := wireguard
 
+PRODUCT_COPY_FILES += \
+    vendor/extra/rootdir/etc/init/ttl.rc:system/etc/init/ttl.rc
+
 # Themes
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
