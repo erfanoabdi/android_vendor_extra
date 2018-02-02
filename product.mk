b01d8200932adc9e37af08cc8ad27e9f5489ffec
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-02 17:37:49 -0600

    extra: copy swipe lib for LatinIME

--- product.mk
+++ product.mk
@@ -6,6 +6,9 @@ $(call prepend-product-if-exists, vendor/invisiblek/product.mk)
 ifneq ($(filter lineage_marlin lineage_mata lineage_nash lineage_sailfish,$(TARGET_PRODUCT)),)
 GAPPS_VARIANT := nano
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
+
+PRODUCT_COPY_FILES += \
+  vendor/opengapps/sources/arm/lib/23/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 TARGET_EXTRA_KERNEL_MODULES := wireguard
