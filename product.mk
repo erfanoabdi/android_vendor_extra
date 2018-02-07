da0339b2a4c32341892273e3b6e07013e5be92fa
Erfan Abdi <erfangplus@gmail.com>
2018-02-07 13:36:59 +0300

    remove invisiblek stuffs
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- product.mk
+++ product.mk
@@ -1,16 +1,6 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 PRODUCT_PACKAGES += auditd
 
-$(call prepend-product-if-exists, vendor/invisiblek/product.mk)
-
-ifneq ($(filter lineage_marlin lineage_mata lineage_nash lineage_sailfish,$(TARGET_PRODUCT)),)
-GAPPS_VARIANT := nano
-$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
-
-PRODUCT_COPY_FILES += \
-  vendor/opengapps/sources/arm/lib/23/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-endif
-
 TARGET_EXTRA_KERNEL_MODULES := wireguard
 
 PRODUCT_COPY_FILES += \
