e7f3c1807779e1db86e031baad648840a2ec4124
Erfan Abdi <erfangplus@gmail.com>
2018-08-18 16:02:54 +0430

    cleanup more
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- product.mk
+++ product.mk
@@ -1,14 +1,7 @@
-PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-PRODUCT_PACKAGES += auditd
-
-TARGET_EXTRA_KERNEL_MODULES := wireguard
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/rootdir/etc/init/ttl.rc:system/etc/init/ttl.rc
-
 # Themes
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
 PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.overlay.photness=true
 PRODUCT_PACKAGES += \
-    PixelTheme
+    PixelTheme \
+    P-Hotness
