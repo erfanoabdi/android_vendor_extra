7b07f796e316d039bd0a8af2647b266bf057265b
Erfan Abdi <erfangplus@gmail.com>
2018-09-19 17:03:12 +0300

    use google sans for setting search bar
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- product.mk
+++ product.mk
@@ -1,4 +1,6 @@
 # Themes
+DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
 PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
 PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.overlay.photness=true
