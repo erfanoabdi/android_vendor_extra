788db6147d1db0705eb00aa399d9087c9e4ac138
Erfan Abdi <erfangplus@gmail.com>
2020-04-18 12:58:48 +0430

    Build PixelTheme as product rro module

--- pixel-theme/Android.mk
+++ pixel-theme/Android.mk
@@ -2,13 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-
+LOCAL_RRO_THEME := Pixel
 LOCAL_PACKAGE_NAME := PixelTheme
 LOCAL_SDK_VERSION := current
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
-
+LOCAL_PRODUCT_MODULE := true
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-
-include $(BUILD_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
